1e3b6be7056c886d839984f22b40f79389541a74
leldenor <norenberga19@outlook.lv>
2020-11-08 21:33:08 +0100

    This

--- FamilyInformation/Data/IUserService.cs
+++
@@ -1,9 +0,0 @@
-﻿using FamilyInformation.Models;
-
-namespace FamilyInformation.Data
-{
-    public interface IUserService
-    {
-        User ValidateUser(string userName, string password);
-    }
-}
